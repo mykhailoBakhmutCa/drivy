6f888a958ce4f038859d7dfe4c2fef9acb65128b
mykhailo.bakhmut <mykhailo.bakhmut.ca@gmail.com>
2025-11-02 13:10:08 -0500

    RentalCalculator.php changed for calculateTimePrice with discount

--- src/RentalCalculator.php
+++ src/RentalCalculator.php
@@ -23,7 +23,7 @@ class RentalCalculator
 
             $days = $startDate->diff($endDate)->days + 1;
 
-            $timePrice = $days * $car['price_per_day'];
+            $timePrice = $this->calculateTimePrice($days, $car['price_per_day']);
             $distancePrice = $rental['distance'] * $car['price_per_km'];
 
             $totalPrice = $timePrice + $distancePrice;
@@ -35,5 +35,23 @@ class RentalCalculator
         }
 
         return ['rentals' => $outputRentals];
+    }
+
+    private function calculateTimePrice(int $days, int $pricePerDay): int
+    {
+        $totalPrice = 0;
+
+        for ($i = 1; $i <= $days; $i++) {
+            $dailyRate = match (true) {
+                $i == 1 => 1,
+                $i <= 4 => 0.9,
+                $i <= 10 => 0.7,
+                default => 0.5,
+            };
+
+            $totalPrice += $dailyRate * $pricePerDay;
+        }
+
+        return (int)$totalPrice;
     }
 }
